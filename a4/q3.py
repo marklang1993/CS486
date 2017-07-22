f8f4200bee51714e3a5c40572747b171e110458d
Lang Chen <marklang1993@gmail.com>
2017-07-21 20:19:44 -0400

    a4: Add plot in q3.

--- a4/q3.py
+++ a4/q3.py
@@ -1,10 +1,15 @@
 import gym
 from gym import spaces
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
 import DQN
 
 env = gym.make('CartPole-v0')
 dqn = DQN.DQN_Replay()
-for i_episode in range(1000):
+discount_reward = np.zeros(1000)
+for i_episode in range(10):
     observation = env.reset()
     for step in range(500):
         #env.render()
@@ -18,9 +23,21 @@ for i_episode in range(1000):
         dqn.train()
         # iterate
         observation = new_observation
-
+        # check
         if done:
             print("Episode finished after {} timesteps".format(step+1))
             break
 
+    # calculate discounted reward
+    if i_episode == 0:
+        discount_reward[i_episode] = reward * math.pow(dqn.gamma, i_episode)
+    else:
+        discount_reward[i_episode] = reward * math.pow(dqn.gamma, i_episode) + discount_reward[i_episode - 1]
+    # notify
     dqn.episode_complete()
+    
+# plot
+plt.plot(np.arange(discount_reward.size), discount_reward)
+plt.ylabel('Discounted Reward')
+plt.xlabel('Episodes')
+plt.show()
